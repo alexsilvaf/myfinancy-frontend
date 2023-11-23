14b3dca69e29efa1e893cfea762436c3dd50bd4a
Alex Ferreira <alex.120999@outlook.com.br>
2023-11-23 13:06:23 -0300

    changing path name to user-account and improving the layou

---
+++ src/app/components/user-account/user-account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-account',
+  templateUrl: './user-account.component.html',
+  styleUrl: './user-account.component.scss'
+})
+export class UserAccountComponent {
+}
